08d90e42238606ff459a81149ecd0fe77d458c40
SergeyPyt <virvich.serg.yandex.by>
2023-08-02 14:35:39 +0300

    7 new file

---
+++ constants.py
@@ -0,0 +1,11 @@
+import string
+
+LETTERS = string.ascii_lowercase
+
+LIST_WORDS = ["work", "milk", "butter"]
+PICTURE_DICTIONARY = {
+    "image1.jpg": ["work", "milk"],
+    "image2.jpg": ["work", "butter"],
+    "image3.jpg": ["milk", "butter"],
+    "image4.jpg": ["butter", "work"]
+       }
